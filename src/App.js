97db65af856af91c1215f5e38de7d7ba15e66049
gitlodey <gitlodey@gmail.com>
2021-03-29 23:28:30 +0300

    add react ui

---
+++ src/App.js
@@ -0,0 +1,12 @@
+import './App.css';
+import {Checkout} from "./components/Checkout/Checkout";
+
+function App() {
+  return (
+    <div className="App container">
+      <Checkout/>
+    </div>
+  );
+}
+
+export default App;
